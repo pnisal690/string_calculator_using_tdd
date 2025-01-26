3aad621ba0e0b68f59b91798e3fed71ce141092f
prashant nisal <pnisal690@gmail.com>
2025-01-27 00:45:29 +0530

    [task-2] possible simple test cases and code

--- spec/string_addition_spec.rb
+++ spec/string_addition_spec.rb
@@ -5,6 +5,29 @@ RSpec.describe 'String Operations' do
     it 'returns 0 for empty' do
       expect(add("")).to eq(0)
     end
+
+    it 'returns a numebr itself if input contains single number' do
+      expect(add("1")).to eq(1)
+    end
+
+    it 'returns sum of two numbers if input contains two comma separated numbers' do
+      expect(add("1, 2")).to eq(3)
+    end
+
+    it 'returns sum of all numbers if input contains comma separated numbers' do
+      expect(add("1, 2, 3, 4, 5")).to eq(15)
+    end
+
+    it 'returns sum of two numbers if input contains new line in between two numbers' do
+      expect(add("1\n2")).to eq(3)
+      expect(add("\n2,1")).to eq(3)
+      expect(add("1,2\n")).to eq(3)
+      expect(add("1\n,2,3\n4,5")).to eq(15)
+    end
+
+    it 'Numbers bigger than 1000 should be ignored' do 
+      expect(add("2,1002")).to eq(2)
+    end
   end
 end
 
--- string_addition.rb
+++ string_addition.rb
@@ -1,3 +1,14 @@
 def add(input)
   return 0 if input.empty?
+
+  input = input.gsub("\\n", "\n") # Sanitize input
+
+  # Default delimiter splitting: commas and newlines
+  sections = input.split(/,|\n/)
+
+  # Extract numbers, convert to integers, and filter numbers > 1000
+  numbers = sections.flat_map { |section| section.scan(/-?\d+/) }.map(&:to_i).reject { |n| n > 1000 }
+
+  # Return the sum
+  numbers.sum
 end
